def7f25b4b1bc716082060be5253a42ff407d8a2
Sabrina Rashid <sabrina@sprwforge.com>
2025-04-24 19:30:35 +0600

    key feature section  is  responsive for tab view

--- app/components/keyfeatures/Card.tsx
+++ app/components/keyfeatures/Card.tsx
@@ -51,7 +51,7 @@ const Card = () => {
             <h1 className="text-[40px] md:text-[60px] text-center font-semibold md:leading-[110.00000001%]">
                 Key features of <br/> crymbo pay
             </h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-[30px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-[30px]">
                 {data.map((card) => (
 
                     <div
