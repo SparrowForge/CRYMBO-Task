c3ec20440550e099475081f14e2ddb7ab867260c
Sabrina Rashid <sabrina@sprwforge.com>
2025-04-29 21:28:05 +0600

    footer link hover effect added

--- app/components/Footer/MainFooter.tsx
+++ app/components/Footer/MainFooter.tsx
@@ -6,7 +6,7 @@ import { MdOutlineMailOutline } from 'react-icons/md';
 const MainFooter = () => {
     return (
         <div className='wapper md:mx-20 lg:mx-0'>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-between text-center md:text-start md:gap-x-40 lg:gap-0">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-between text-center md:text-start md:gap-x-40 lg:gap-20">
 
                 <div className="mb-6 lg:mb-0">
                     <h3 className="text-[20px] lg:text-[16px] font-bold mb-6">Company</h3>
@@ -21,9 +21,9 @@ const MainFooter = () => {
 
                 <div className="mb-6 lg:mb-0">
                     <h3 className="text-[20px] lg:text-[16px] font-bold mb-6">Products</h3>
-                    <ul className="space-y-2 text-[16px]">
-                        <li><a href="#" className="text-[#3E4440]">Cymbro Oracle</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Cymbro Connect</a></li>
+                    <ul className="space-y-2 text-[16px] ">
+                        <li className='customLink'><a href="#" className="">Cymbro Oracle</a></li>
+                        <li className='customLink'><a href="#" className="">Cymbro Connect</a></li>
                     </ul>
                 </div>
 
@@ -31,12 +31,12 @@ const MainFooter = () => {
                 <div className="mb-6 lg:mb-0">
                     <h3 className="text-[20px] lg:text-[16px] font-bold mb-6">Use Cases</h3>
                     <ul className="space-y-2 text-[16px]">
-                        <li><a href="#" className="text-[#3E4440]">Launch crypto products, fast</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Scale operations & tech with ease</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Control multiple wallets & custodians</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Improve Capital Efficiency</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Intelligent Risk Management</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Know your data</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Launch crypto products, fast</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Scale operations & tech with ease</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Control multiple wallets & custodians</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Improve Capital Efficiency</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Intelligent Risk Management</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Know your data</a></li>
                     </ul>
                 </div>
 
@@ -44,10 +44,10 @@ const MainFooter = () => {
                 <div className="mb-6 lg:mb-0">
                     <h3 className="text-[20px] lg:text-[16px] font-bold mb-6">Developer</h3>
                     <ul className="space-y-2 text-[16px]">
-                        <li><a href="#" className="text-[#3E4440]">Docs</a></li>
-                        <li><a href="#" className="text-[#3E4440]">API Reference</a></li>
-                        <li><a href="#" className="text-[#3E4440]">On Cymbro Marketplace</a></li>
-                        <li><a href="#" className="text-[#3E4440]">Privacy Policy</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Docs</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">API Reference</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">On Cymbro Marketplace</a></li>
+                        <li className='customLink'><a href="#" className="text-[#3E4440]">Privacy Policy</a></li>
                     </ul>
                 </div>
             </div>
